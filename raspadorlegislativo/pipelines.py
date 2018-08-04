f88b505054f048856f7fe2d194f25963d37f2cac
Eduardo Cuducos <cuducos@gmail.com>
2018-08-04 15:22:23 -0300

    Configura envio de dados da tramitação para o Radar

--- raspadorlegislativo/pipelines.py
+++ raspadorlegislativo/pipelines.py
@@ -2,7 +2,8 @@ import logging
 
 from requests import post
 
-from raspadorlegislativo import settings
+from raspadorlegislativo.items import Bill, Event
+from raspadorlegislativo.settings import RASPADOR_API_TOKEN, RASPADOR_API_URL
 
 
 log = logging.getLogger(__name__)
@@ -11,24 +12,38 @@ log = logging.getLogger(__name__)
 class RaspadorlegislativoPipeline:
 
     def process_item(self, item, spider):
-        if all((settings.RASPADOR_API_TOKEN, settings.RASPADOR_API_URL)):
+        if all((RASPADOR_API_TOKEN, RASPADOR_API_URL)):
             self.post(item)
 
         return item
 
     def post(self, item):
-        url = f'{settings.RASPADOR_API_URL}projeto/'
-        response = post(url, data=self.serialize(item))
+        response = post(self.endpoint(item), data=self.serialize(item))
         if response.status_code != 201:
-            log.info('Bill not saved via API')
+            log.info(f'{item._class.__name__} not saved via API')
             log.info(response.status_code)
             log.info(response.text)
             return
 
-        log.info('Bill saved via API')
+        log.info(f'{item._class.__name__} saved via API')
 
     def serialize(self, item):
         data = dict(item)
-        data['token'] = settings.RASPADOR_API_TOKEN
-        data['palavras_chave'] = ', '.join(data['palavras_chave'])
+        data['token'] = RASPADOR_API_TOKEN
+
+        if isinstance(item, Bill):
+            data['palavras_chave'] = ', '.join(data['palavras_chave'])
+
         return data
+
+    def endpoint(self, item):
+        endpoint = None
+        if isinstance(item, Bill):
+            endpoint = 'projeto/'
+        elif isinstance(item, Event):
+            endpoint = 'tramitacao/'
+
+        if not endpoint:
+            raise ValueError(f'No endpoint set for {item._class}')
+
+        return f'{RASPADOR_API_URL}{endpoint}'
