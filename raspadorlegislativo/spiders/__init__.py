12f8b10153ab6d4f224ba1e04847a58647ad569f
Eduardo Cuducos <cuducos@gmail.com>
2018-08-07 13:06:15 -0300

    Remove chamada para API do Radar ao final dos Spiders
    Como o processamento dos dados no radar é assíncrono, não faz mais sentido essa funcionalidade

--- raspadorlegislativo/spiders/__init__.py
+++ raspadorlegislativo/spiders/__init__.py
@@ -1,35 +1,12 @@
 import os
 from contextlib import contextmanager
-from json import JSONDecodeError
 from tempfile import mkstemp
 
 from PyPDF2 import PdfFileReader
-from requests import post
 from scrapy import Spider as OriginalSpider
-
-from raspadorlegislativo import settings
 
 
 class Spider(OriginalSpider):
-
-    def close(self, spider):
-        if not all((settings.RASPADOR_API_TOKEN, settings.RASPADOR_API_URL)):
-            return
-
-        data = {
-            'token': settings.RASPADOR_API_TOKEN,
-            'start_time': spider.crawler.stats.get_value('start_time'),
-            'origem': 'CA' if self.name == 'camara' else 'SE'
-        }
-        url = f'{settings.RASPADOR_API_URL}fim-da-raspagem/'
-        response = post(url, data=data)
-
-        try:
-            content = response.json()
-        except JSONDecodeError:
-            content = response.text
-
-        spider.logger.info(content)
 
     @contextmanager
     def text_from_pdf(self, pdf_in_bytes):
