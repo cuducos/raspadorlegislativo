143a2075e103439c6f01fab687e939699354bf28
Eduardo Cuducos <cuducos@gmail.com>
2018-08-24 15:04:18 -0300

    Desabilita `dupefilter`
    Requisições para `local` da Câmara são repetidas

--- raspadorlegislativo/settings.py
+++ raspadorlegislativo/settings.py
@@ -29,6 +29,7 @@ ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 32
+DUPEFILTER_CLASS = 'scrapy.dupefilters.BaseDupeFilter'  # disable dupefilter
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
